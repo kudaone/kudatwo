3d35dab6ec59c4590272da9e249f38bb91d1ff5d
kudaone <101389722+kudaone@users.noreply.github.com>
2023-07-04 19:51:12 +0700

    Update run.js

--- run.js
+++ run.js
@@ -1,8 +1,12 @@
 // Function to click the button
 function clickButton() {
-  var button = document.querySelector('button[type="submit"]');
-  button.click();
+  var buttons = document.getElementsByTagName('button');
+  var index = 3; // Index of the button to click (0-based index)
 
+  if (index < buttons.length) {
+    buttons[index].click();
+  }
+  
   // Schedule the next click after 60 minutes
   setTimeout(clickButton, 60 * 60 * 1000);
 }
